3768d8aeec7b91f5bbd81a9b582ffd1549da056e
Tobias Leinss <caruso33@web.de>
2021-02-14 18:39:06 +0100

    Start intro to game playing with gamestate class

---
+++ udacity_artificial_intelligence/2_intro_game_playing/gamestate.py
@@ -0,0 +1,140 @@
+import copy
+
+
+class GameState:
+    """
+    Attributes
+    ----------
+    _board: list(list)
+        Represent the board with a 2d array _board[x][y]
+        where open spaces are 0 and closed spaces are 1
+        and a coordinate system where [0][0] is the top-
+        left corner, and x increases to the right while
+        y increases going down (this is an arbitrary
+        convention choice -- there are many other options
+        that are just as good)
+
+    _parity: bool
+        Keep track of active player initiative (which
+        player has control to move) where 0 indicates that
+        player one has initiative and 1 indicates player two
+
+    _player_locations: list(tuple)
+        Keep track of the current location of each player
+        on the board where position is encoded by the
+        board indices of their last move, e.g., [(0, 0), (1, 0)]
+        means player one is at (0, 0) and player two is at (1, 0)
+
+    Considerations:
+        - Board coordinates are situated at the top - left corner
+        - Position structure is based on tuples (x: columns,y: rows)
+        - Movements are numbered starting from: 'upwards' till: 'diagonal upwards - left', clockwise
+    """
+
+    def __init__(self, dim_x=3, dim_y=2):
+        self._board = [[0] * dim_y for _ in range(dim_x)]
+        self._board[-1][-1] = "/"
+        self._player_locations = [None, None]
+        self._parity = 0  # player one
+
+    def get_legal_moves(self):
+        """Return a list of all legal moves available to the
+        active player.  Each player should get a list of all
+        empty spaces on the board on their first move, and
+        otherwise they should get a list of all open spaces
+        in a straight line along any row, column or diagonal
+        from their current position. (Players CANNOT move
+        through obstacles or blocked squares.) Moves should
+        be a pair of integers in (column, row) order specifying
+        the zero-indexed coordinates on the board.
+        """
+        legal_moves = []
+
+        num_row = len(self._board)
+        num_col = len(self._board[0])
+
+        move_directions = [
+            (0, -1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 1),
+            (-1, 0),
+            (-1, -1),
+        ]
+
+        def move_if_legal(current_direction, board_location=()):
+            nonlocal legal_moves
+
+            player_location = self._player_locations[self._parity]
+
+            if not player_location:
+                legal_moves = [
+                    (x, y)
+                    for x in range(num_row)
+                    for y in range(num_col)
+                    if self._board[x][y] == 0
+                ]
+
+                return  # all board positions
+
+            loc_zip = (
+                zip(board_location, current_direction)
+                if board_location
+                else zip(player_location, current_direction)
+            )
+
+            new_loc = tuple(map(sum, loc_zip))
+            new_row_loc, new_col_loc = new_loc
+
+            # Outside of board
+            if new_row_loc < 0 or new_row_loc >= num_row:
+                return
+
+            if new_col_loc < 0 or new_col_loc >= num_col:
+                return
+
+            # Place occupied
+            if self._board[new_row_loc][new_col_loc] != 0:
+                return
+
+            # is legal move
+            legal_moves.append(new_loc)
+
+            # check ongoing direction
+            return move_if_legal(current_direction, new_loc)
+
+        for move_direction in move_directions:
+            move_if_legal(move_direction)
+
+        return legal_moves
+
+    def forecast_move(self, move):
+        """Return a new board object with the specified move
+        applied to the current game state.
+
+        Parameters
+        ----------
+        move: tuple
+            The target position for the active player's next move
+            (e.g., (0, 0) if the active player will move to the
+            top-left corner of the board)
+        """
+
+        new_board = copy.deepcopy(self)
+
+        new_board._player_locations[new_board._parity] = move
+
+        new_board._board[move[0]][move[1]] = 1
+
+        new_board._parity = int(not new_board._parity)
+
+        return new_board
+
+    def __str__(self) -> str:
+        return f"""
+        Board: {self._board}
+        Parity: {self._parity}
+        Player locations: {self._player_locations}
+        """
---
+++ udacity_artificial_intelligence/2_intro_game_playing/test_gamestate.py
@@ -0,0 +1,84 @@
+import unittest
+import gamestate
+
+from gamestate import *
+
+
+def test_game_class():
+    print("Creating empty game board...")
+    g = GameState()
+
+    print("Getting legal moves for player 1...")
+    p1_empty_moves = g.get_legal_moves()
+    print("Found {} legal moves.".format(len(p1_empty_moves or [])))
+
+    print("Applying move (0, 0) for player 1...")
+    g1 = g.forecast_move((0, 0))
+
+    print("Getting legal moves for player 2...")
+    p2_empty_moves = g1.get_legal_moves()
+    if (0, 0) in set(p2_empty_moves):
+        print(
+            "Failed\n  Uh oh! (0, 0) was not blocked properly when "
+            + "player 1 moved there."
+        )
+    else:
+        print("Everything looks good!")
+
+
+class TestGameState(unittest.TestCase):
+    def test_initializer_default(self):
+        board = [[0] * 2 for _ in range(3)]
+        board[2][1] = "/"
+        game = gamestate.GameState()
+
+        self.assertEqual(game._board, board)
+
+    def test_forecast_move(self):
+        game = gamestate.GameState()
+        future_game_1 = game.forecast_move((0, 0))
+        future_game_2 = future_game_1.forecast_move((1, 1))
+        # Player 0 checks
+        self.assertEqual(future_game_1._board[0][0], 1)
+        self.assertEqual(future_game_1._parity, 1)
+        self.assertEqual(future_game_1._player_locations[0], (0, 0))
+        # Player 1 checks
+        self.assertEqual(future_game_2._board[1][1], 1)
+        self.assertEqual(future_game_2._parity, 0)
+        self.assertEqual(future_game_2._player_locations[1], (1, 1))
+
+    def test_get_legal_moves(self):
+        game_1 = gamestate.GameState()
+        future_game_1_1 = game_1.forecast_move((0, 0))  # Player 1
+        future_game_1_2 = future_game_1_1.forecast_move((1, 1))  # Player 2
+        future_game_1_3 = future_game_1_2.forecast_move((0, 1))  # Player 1
+
+        game_2 = gamestate.GameState()
+        future_game_2_1 = game_2.forecast_move((1, 1))  # Player 1
+        future_game_2_2 = future_game_2_1.forecast_move((0, 1))  # Player 2
+
+        game_3 = gamestate.GameState()
+        future_game_3_1 = game_3.forecast_move((1, 0))  # Player 1
+        future_game_3_2 = future_game_3_1.forecast_move((0, 0))  # Player 2
+
+        # Initial case:
+        self.assertEqual(
+            game_1.get_legal_moves(), [(0, 0), (0, 1), (1, 0), (1, 1), (2, 0)]
+        )
+        self.assertEqual(
+            future_game_1_1.get_legal_moves(), [(0, 1), (1, 0), (1, 1), (2, 0)]
+        )
+
+        # Other cases:
+        self.assertEqual(future_game_1_2.get_legal_moves(), [(1, 0), (2, 0), (0, 1)])
+        self.assertEqual(future_game_1_3.get_legal_moves(), [(1, 0), (2, 0)])
+
+        self.assertEqual(future_game_2_2.get_legal_moves(), [(1, 0), (2, 0), (0, 0)])
+
+        self.assertEqual(future_game_3_2.get_legal_moves(), [(2, 0), (1, 1), (0, 1)])
+
+
+if __name__ == "__main__":
+    # test_game_class()
+
+    unittest.main()
